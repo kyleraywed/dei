049481c1e7f09e3028952170abd43158c8aa4147
Kyle Weddermann <themilkman311@gmail.com>
2025-12-08 07:12:23 -0500

    Implement example demonstrating WithDeepClone(). Clean doc in examples/primes.

---
+++ examples/deepClone/main.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+	- Demonstrate WithDeepClone()
+*/
+
+import (
+	"fmt"
+	"maps"
+	"slices"
+
+	"github.com/kyleraywed/dee"
+)
+
+type person struct {
+	name string
+	tags []string
+	meta map[int]string
+}
+
+func main() {
+	p := []person{
+		{
+			name: "Kyle",
+			tags: []string{"x", "y", "z"},
+			meta: map[int]string{
+				1: "one",
+				2: "two",
+				3: "three",
+			},
+		},
+	}
+
+	var enum dee.Dee[person]
+
+	// Without this, Apply() will affect p.
+	// It can be placed anywhere before Apply().
+	enum.WithDeepClone(func(value person) person {
+		out := value
+		out.tags = slices.Clone(value.tags)
+		out.meta = maps.Clone(value.meta)
+		return out
+	})
+
+	enum.Map(func(value person) person {
+		value.tags = append(value.tags, "a")
+		value.meta[4] = "four"
+		return value
+	})
+
+	new := enum.Apply(p)
+
+	fmt.Println("New:\t", new)
+	fmt.Println("Old:\t", p)
+}
